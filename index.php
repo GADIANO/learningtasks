7f08a3c73da58d63f574d2dfa6df07aff00baa45
GADIANO <100126234+GADIANO@users.noreply.github.com>
2022-05-20 00:38:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
       <li><a href="index.html">Learning Task 3</a></li>
       <li><a href="CLE1MT.html">Midterm Exam</a></li>
       <li><a href="LT4/index.html">Learning Task 4</a></li>
+      <li><a href="LT5/index.html">Learning Task 5</a></li>
  </ul>
   </body>
 </html>
